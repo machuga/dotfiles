6606fd19b87224f09ac4fd8206634cb14dec3836
Matthew Machuga <mmachuga@thinkthroughmath.com>
2014-04-12 20:30:43 -0400

    Add two missing init files, recipes, and tweak gitignore

---
+++ .emacs.d/inits/init-evil-leader.el
@@ -0,0 +1,3 @@
+(global-evil-leader-mode)
+
+(evil-leader/set-key "f" 'fiplr-find-file)
---
+++ .emacs.d/inits/init-fiplr.el
@@ -0,0 +1,2 @@
+(setq fiplr-ignored-globs '((directories (".git" ".svn" "bower_components" "node_modules"))
+                            (files ("*.jpg" "*.png" "*.zip" "*.elc" "*~"))))
