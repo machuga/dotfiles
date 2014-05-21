e2a9a8a523ec2f92adec66ea6dde8ffc380c885d
Matthew Machuga <mmachuga@thinkthroughmath.com>
2014-05-21 09:34:18 -0400

    Adding emacs term-bindings file

---
+++ .emacs.d/term-bindings.el
@@ -0,0 +1,22 @@
+(require 'term)
+
+(defun jnm/term-toggle-mode ()
+  "Toggles term between line mode and char mode"
+  (interactive)
+  (if (term-in-line-mode)
+      (term-char-mode)
+    (term-line-mode)))
+
+(define-key term-mode-map (kbd "C-c C-j") 'jnm/term-toggle-mode)
+(define-key term-mode-map (kbd "C-c C-k") 'jnm/term-toggle-mode)
+
+(define-key term-raw-map (kbd "C-c C-j") 'jnm/term-toggle-mode)
+(define-key term-raw-map (kbd "C-c C-k") 'jnm/term-toggle-mode)
+
+(defadvice ansi-term (after advise-ansi-term-coding-system)
+    (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+
+(ad-activate 'ansi-term)
+
+(provide 'term-bindings)
+
