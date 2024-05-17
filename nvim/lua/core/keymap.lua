43ffb763500ecd1678f288f7bfa7dbfecf002013
Matt Machuga <matt.machuga@oysterhr.com>
2024-05-17 06:43:17 -0400

    New nvim config

---
+++ nvim/lua/core/keymap.lua
@@ -0,0 +1,34 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Copy to system paste buffer
+vim.keymap.set("n", "<leader>y", '"*y', { desc = "Copy to system paste buffer" })
+
+-- Window movements
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Focus window to the left" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Focus window below" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Focus window above" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Focus window to the right" })
+vim.keymap.set("n", "<leader>wH", "<C-w>H", { desc = "Move window to the left" })
+vim.keymap.set("n", "<leader>wJ", "<C-w>J", { desc = "Move window up" })
+vim.keymap.set("n", "<leader>wK", "<C-w>K", { desc = "Move window down" })
+vim.keymap.set("n", "<leader>wL", "<C-w>L", { desc = "Move window to the right" })
+
+-- Buffer movements
+vim.keymap.set("n", "<leader>bn", ":bn<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bp<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bd", ":bd<cr>", { desc = "Delete buffer" })
+
+-- Config management
+vim.keymap.set("n", "<leader>cf", ":vs ~/.config/nvim/init.lua<cr>", { desc = "Split edit config" })
+vim.keymap.set("n", "<leader>cr", ":source ~/.config/nvim/init.lua<cr>", { desc = "Re-source config" })
+
+-- Editing convenience
+vim.keymap.set("n", "<leader>e", ":edit <C-R>=expand('%:p:h') . '/' <CR>", { desc = "Edit sibling file" })
+vim.keymap.set("n", "<leader>v", ":view <C-R>=expand('%:p:h') . '/' <CR>", { desc = "View sibling file" })
+vim.keymap.set("n", "<leader>sp", ":sp <C-R>=expand('%:p:h') . '/' <CR>",
+  { desc = "Edit sibling file in horizontal split" })
+vim.keymap.set("n", "<leader>vs", ":vs <C-R>=expand('%:p:h') . '/' <CR>",
+  { desc = "Edit sibling file in vertical split" })
+
+vim.keymap.set("n", "<leader>o", ':set nopaste<cr>', { desc = "Remove search highlighting" })
