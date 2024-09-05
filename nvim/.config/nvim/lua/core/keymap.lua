af9472566ea29429febb379bccca15d797d55110
Matt Machuga <machuga@gmail.com>
2024-09-05 05:44:07 -0400

    Updating nvim keybindings

--- nvim/.config/nvim/lua/core/keymap.lua
+++ nvim/.config/nvim/lua/core/keymap.lua
@@ -33,3 +33,19 @@ vim.keymap.set("n", "<leader>vs", ":vs <C-R>=expand('%:p:h') . '/' <CR>",
 
 vim.keymap.set("n", "<leader>o", ':set nopaste<cr>', { desc = "Remove search highlighting" })
 vim.keymap.set("i", "kj", '<esc>', { desc = "Quick exit" })
+
+vim.api.nvim_exec([[
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Rename Current File
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+function! RenameFile()
+    let old_name = expand('%')
+    let new_name = input('New file name: ', expand('%'), 'file')
+    if new_name != '' && new_name != old_name
+        exec ':saveas ' . new_name
+        exec ':silent !rm ' . old_name
+        redraw!
+    endif
+endfunction
+map <leader>rN :call RenameFile()<cr>
+]], false)
--- nvim/.config/nvim/lua/core/lsp.lua
+++ nvim/.config/nvim/lua/core/lsp.lua
@@ -1,3 +1,37 @@
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+--vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+--vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- Unused for now
+--local servers = {
+--  ruby_lsp = {
+--    cmd = { "bundle", "exec", "ruby-lsp" }
+--  },
+--  lua_ls = {
+--    Lua = {
+--      diagnostics = {
+--        -- Get the language server to recognize the `vim` global
+--        globals = { "vim" },
+--      },
+--      workspace = {
+--        -- Make the server aware of Neovim runtime files
+--        library = vim.api.nvim_get_runtime_file("", true),
+--      },
+--      -- Do not send telemetry data containing a randomized but unique identifier
+--      telemetry = {
+--        enable = false,
+--      }
+--    }
+--  }
+--}
+
+-- Consider adding the snippet from: https://shopify.github.io/ruby-lsp/EDITORS_md.html
+--
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
@@ -6,9 +40,9 @@ require("mason-lspconfig").setup({
     "cssls",
     "jsonls",
     "tsserver",
-    "solargraph",
+    --"solargraph",
     "ruby_lsp",
-    "rubocop",
+    --"rubocop",
     "taplo",
     "yamlls",
     "vimls",
@@ -16,6 +50,65 @@ require("mason-lspconfig").setup({
     "graphql"
   },
 })
+
+--require("mason-lspconfig").setup_handlers {
+--  function(server_name)
+--    require("lspconfig")[server_name].setup {
+--      capabilities = capabilities,
+--      --on_attach = on_attach,
+--      settings = servers[server_name],
+--      filetypes = (servers[server_name] or {}).filetypes,
+--    }
+--  end
+--}
+--
+---- LSP settings.
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+  -- NOTE: Remember that lua is a real programming language, and as such it is possible
+  -- to define small helper and utility functions so you don't have to repeat yourself
+  -- many times.
+  --
+  -- In this case, we create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>ltd', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>lds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>lrn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>lca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Lesser used LSP functionality
+  nmap('<leader>lwa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>lwr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>lws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>lwl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    if vim.lsp.buf.format then
+      vim.lsp.buf.format()
+    elseif vim.lsp.buf.formatting then
+      vim.lsp.buf.formatting()
+    end
+  end, { desc = 'Format current buffer with LSP' })
+end
 
 require('lspconfig').lua_ls.setup {
   Lua = {
@@ -31,11 +124,15 @@ require('lspconfig').lua_ls.setup {
     telemetry = {
       enable = false,
     },
+    capabilities = capabilities,
+    on_attach = on_attach
   },
 }
 
 require('lspconfig').ruby_lsp.setup {
-  cmd = { "./bin/bundle", "exec", "ruby-lsp" }
+  cmd = { "./bin/bundle", "exec", "ruby-lsp" },
+  capabilities = capabilities,
+  on_attach = on_attach
 }
 
 --require('lspconfig').rubocop.setup {
